8b781f8fb773025b326f2e7a5c934b9e40b1d7b5
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-07 22:19:50 +0900

    level3_7_11021 copy

--- backjoon/prac/app.js
+++ backjoon/prac/app.js
@@ -1,12 +1,14 @@
 // "/dev/stdin"
 
-let input = require("fs").readFileSync("./input.txt").toString().trim();
-input = +input;
+let input = require("fs").readFileSync("./input.txt").toString().split("\n");
 
-let answer = "";
+const testNum = +input[0];
 
-for (let i = input; i >= 1; i--) {
-  answer += i + "\n";
+for (let i = 1; i <= testNum; i++) {
+  const arr = input[i].split(" ").map((val) => +val);
+
+  let a = arr[0];
+  let b = arr[1];
+
+  console.log(`Case #${i}: ${a + b}`);
 }
-
-console.log(answer);
