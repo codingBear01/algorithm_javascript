5192cda4ed15559396032e6aa9ab7c62d9a1b565
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-07 23:26:29 +0900

    level3_10_2439

--- backjoon/level3_for/level3_10_2439.js
+++ backjoon/level3_for/level3_10_2439.js
@@ -1,6 +1,8 @@
 // 문제
 // 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
 
+// 하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+
 // 입력
 // 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
 
@@ -10,17 +12,23 @@
 // 예제 입력 1
 // 5
 // 예제 출력 1
-// *
-// **
-// ***
-// ****
+//     *
+//    **
+//   ***
+//  ****
 // *****
 
 let input = require("fs").readFileSync("/dev/stdin").toString();
+num = +input;
 
 let star = "";
+let blank = "";
 
-for (let i = 0; i < input; i++) {
+for (let i = 1; i <= num; i++) {
   star += "*";
-  console.log(star);
+  for (let j = 0; j < num - i; j++) {
+    blank += " ";
+  }
+  console.log(blank + star);
+  blank = "";
 }
--- backjoon/prac/app.js
+++ backjoon/prac/app.js
@@ -1,10 +1,16 @@
 // "/dev/stdin"
 
 let input = require("fs").readFileSync("./input.txt").toString();
+num = +input;
 
 let star = "";
+let blank = "";
 
-for (let i = 0; i < input; i++) {
+for (let i = 1; i <= num; i++) {
   star += "*";
-  console.log(star);
+  for (let j = 0; j < num - i; j++) {
+    blank += " ";
+  }
+  console.log(blank + star);
+  blank = "";
 }
