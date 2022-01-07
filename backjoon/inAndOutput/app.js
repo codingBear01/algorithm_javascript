e24ced4fbba3752c3ec12517fc5328340ae21f5e
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-07 12:29:15 +0900

    level2_4_14681

--- backjoon/inAndOutput/app.js
+++ backjoon/inAndOutput/app.js
@@ -1,11 +1,30 @@
 // 2000
 // "/dev/stdin"
 
-const fs = require("fs");
-const A = fs.readFileSync("./input.txt").toString().split(" ");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-if ((A % 4 == 0 && A % 100 != 0) || A % 400 == 0) {
-  console.log(1);
-} else {
-  console.log(0);
-}
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(parseInt(line));
+}).on("close", function () {
+  ///////////////////////////////
+  const x = input[0];
+  const y = input[1];
+
+  if (x > 0 && y > 0) {
+    console.log(1);
+  } else if (x < 0 && y > 0) {
+    console.log(2);
+  } else if (x < 0 && y < 0) {
+    console.log(3);
+  } else if (x > 0 && y < 0) {
+    console.log(4);
+  }
+  /////////////////////////////
+  process.exit();
+});
