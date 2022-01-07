78e0b5f7983aa373808baad0610bbb302b62ff67
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-07 11:24:44 +0900

    level1_10430

---
+++ backjoon/inAndOutput/app copy.js
@@ -0,0 +1,13 @@
+// 7 3
+
+const fs = require("fs");
+let input = fs.readFileSync("./input.txt").toString().split(" ");
+
+let a = +input[0];
+let b = +input[1];
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
