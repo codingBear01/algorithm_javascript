afcf04400a4705906fa3f9a5ce54cdeb2673308f
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-08 10:38:09 +0900

    level4_3_1110

--- backjoon/prac/app.js
+++ backjoon/prac/app.js
@@ -1,20 +1,20 @@
 // "/dev/stdin"
 
-let input = require("fs")
-  .readFileSync("./input.txt")
-  .toString()
-  .trim()
-  .split("\n");
+let input = require("fs").readFileSync("./input.txt").toString().trim();
 
+let num = input;
+let sum;
 let i = 0;
-let result = "";
 
-while (i <= input.length - 1) {
-  let a = +input[i].split(" ")[0];
-  let b = +input[i].split(" ")[1];
-  result += `${a + b}` + "\n";
-
+while (true) {
   i++;
+
+  sum = Math.floor(num / 10) + (num % 10);
+  num = (num % 10) * 10 + (sum % 10);
+
+  if (input == num) {
+    break;
+  }
 }
 
-console.log(result);
+console.log(i);
