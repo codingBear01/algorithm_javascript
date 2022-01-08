9d3deba03b68397b8b362fc015381aadf70f39d7
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-08 11:40:48 +0900

    level5_2_2562

--- backjoon/prac/app.js
+++ backjoon/prac/app.js
@@ -4,26 +4,11 @@ let input = require("fs")
   .readFileSync("./input.txt")
   .toString()
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((val) => +val);
 
-const num = +input[0];
-const arr = input[1].split(" ").map((val) => +val);
+let max = Math.max.apply(null, input);
+let maxIndex = input.indexOf(max);
 
-// arr.sort((a, b) => a - b);
-// console.log(arr[0], arr[num - 1]);
-
-let min = arr[0];
-let max = arr[0];
-
-for (let i = 0; i < num; i++) {
-  console.log(arr[i]);
-
-  if (min > arr[i]) {
-    min = arr[i];
-  }
-
-  if (max < arr[i]) {
-    max = arr[i];
-  }
-}
-console.log(min, max);
+console.log(max);
+console.log(maxIndex + 1);
