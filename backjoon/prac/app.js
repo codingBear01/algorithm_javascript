5da33ecc91f31484b86e98efed84e1cfb5e82dcf
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-08 13:22:46 +0900

    level5_3_2577

--- backjoon/prac/app.js
+++ backjoon/prac/app.js
@@ -4,18 +4,4 @@ let input = require("fs")
   .readFileSync("./input.txt")
   .toString()
   .trim()
-  .split("\n")
-  .map((val) => +val);
-
-let max = input[0];
-let maxIdx = 0;
-
-for (let i = 1; i < 9; i++) {
-  if (max < input[i]) {
-    max = input[i];
-    maxIdx = i;
-  }
-}
-
-console.log(max);
-console.log(maxIdx + 1);
+  .split("\n");
