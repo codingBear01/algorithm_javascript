8db21d1c6390368dd6e8e4281b2945fdf172dcbd
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-08 09:55:15 +0900

    level4_1_10952

--- backjoon/prac/app.js
+++ backjoon/prac/app.js
@@ -1,19 +1,24 @@
 // "/dev/stdin"
 
-let input = require("fs").readFileSync("./input.txt").toString().split("\n");
+let input = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
-let num = input[0].split(" ");
-
-let N = +num[0];
-let X = +num[1];
-let A = input[1].split(" ");
-
+let i = 0;
 let result = "";
 
-for (let i = 0; i < N; i++) {
-  if (+A[i] < X) {
-    result = result + A[i] + " ";
+while (i <= input.length - 1) {
+  let a = +input[i].split(" ")[0];
+  let b = +input[i].split(" ")[1];
+
+  if (a != 0 || b != 0) {
+    result += `${a + b}` + "\n";
+  } else {
+    break;
   }
+  i++;
 }
 
 console.log(result);
