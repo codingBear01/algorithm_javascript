bbe3e7bca64cd841c2824048d17ee269548099fa
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-08 14:46:26 +0900

    level5_4_3052

--- backjoon/prac/app.js
+++ backjoon/prac/app.js
@@ -4,4 +4,15 @@ let input = require("fs")
   .readFileSync("./input.txt")
   .toString()
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((val) => +val);
+
+let num = input;
+let arr = [];
+
+for (let i = 0; i < num.length; i++) {
+  arr.push(num[i] % 42);
+}
+
+let result = Array.from(new Set(arr));
+console.log(result.length);
