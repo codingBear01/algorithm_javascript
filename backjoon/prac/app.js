ee459157c0e1bb7bed72cb8ef5ce3cc11ea41cb5
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-09 21:03:41 +0900

    level7_3_10809

--- backjoon/prac/app.js
+++ backjoon/prac/app.js
@@ -1,6 +1,18 @@
 // "/dev/stdin"
 
 let fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString();
+let input = fs.readFileSync("./input.txt").toString().split(" ");
 
-console.log(input.charCodeAt(0));
+let resultArr = [];
+let result = "";
+
+for (let i = 97; i <= 122; i++) {
+  let alphabet = String.fromCharCode(i);
+
+  for (let j = 0; j < input.length; j++) {
+    resultArr.push(input[j].indexOf(alphabet));
+  }
+}
+
+result = resultArr.join(" ");
+console.log(result);
