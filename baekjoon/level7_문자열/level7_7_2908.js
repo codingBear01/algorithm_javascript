c0f3154c18c60531bae7757c935b1b4a36afad96
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-10 16:28:01 +0900

    Update level7_7_2908.js

--- baekjoon/level7_문자열/level7_7_2908.js
+++ baekjoon/level7_문자열/level7_7_2908.js
@@ -42,3 +42,15 @@ for (let i = 0; i < input.length; i++) {
 
 let result = arr2[0] > arr2[1] ? arr2[0] : arr2[1];
 console.log(result);
+
+// simple
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ");
+
+const [a, b] = input.map((v) => [...v].reverse().join(""));
+
+console.log(a > b ? a : b);
+// console.log(Math.max(a, b));
