43128428233b6af6efb284ab5271a5d99c8ea061
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-10 16:22:09 +0900

    level7_7_2908

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -1,37 +1,21 @@
 // "/dev/stdin"
+// 734 893
 
-let input = require("fs").readFileSync("./input.txt").toString().toLowerCase();
+let input = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split(" ");
 
-let str = input.toUpperCase();
-let Alphbet = [];
-let arr = [];
+let arr1 = [];
+let arr2 = [];
 
-for (let i = 65; i <= 90; i++) {
-  Alphbet.push(String.fromCharCode(i));
+for (let i = 0; i < input.length; i++) {
+  let reversedNum = input[i].split("").reverse();
+  arr1.push(reversedNum);
+  let newVal = arr1[i].join("");
+  arr2.push(newVal);
 }
 
-for (let i = 0; i < 26; i++) {
-  arr[i] = 0;
-}
-
-for (let i = 0; i < str.length; i++) {
-  for (let j = 0; j < arr.length; j++) {
-    if (str[i] === Alphbet[j]) {
-      arr[j]++;
-    }
-  }
-}
-
-let count = 0;
-
-for (let i = 0; i < arr.length; i++) {
-  if (Math.max.apply(null, arr) === arr[i]) {
-    count++;
-  }
-}
-
-if (count > 1) {
-  console.log("?");
-} else if (count === 1) {
-  console.log(Alphbet[arr.indexOf(Math.max.apply(null, arr))]);
-}
+let result = arr2[0] > arr2[1] ? arr2[0] : arr2[1];
+console.log(result);
