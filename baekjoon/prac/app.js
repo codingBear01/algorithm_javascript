275c056c67806a4c591a06eb467424ef6d0e062a
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-12 08:15:09 +0900

    level8_2_2292

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -1,21 +1,15 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split(" ");
+let input = fs.readFileSync(filePath).toString().trim();
+input = +input;
 
-let A = Number(input[0]);
-let B = Number(input[1]);
-let C = Number(input[2]);
+let range = 1,
+  block = 1;
 
-let N = A / (C - B);
+while (block < input) {
+  block += 6 * range;
 
-let answer = Math.floor(N) + 1;
+  range++;
+}
 
-B >= C ? (answer = -1) : answer;
-
-console.log(answer);
-
-// BREAK-EVEN POINT = C * 판매대수 > (A + (B * 판매대수))
-// 한 대 생산비 = 1000 + 70 * 1 = 1070
-// 한 대 매출 = 170 * 1 = 170
-// 판매대수를 for 돌려야 댐 ㅇㅅㅇ
-// while? for?
+console.log(range);
