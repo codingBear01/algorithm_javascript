f4fa1c6697a484e281cf1de8add871143fb7236c
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-12 14:11:10 +0900

    level8_5_10250

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -1,14 +1,23 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split(" ")
-  .map((val) => +val);
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const A = input[0];
-const B = input[1];
-const V = input[2];
+const T = Number(input.shift());
+for (let k = 0; k < T; k++) {
+  const arr = [];
+  const HWN = input[k].split(" ");
+  const H = Number(HWN[0]); //층
+  const W = Number(HWN[1]); //방
+  const N = Number(HWN[2]); //손님
 
-console.log(Math.ceil(V - B / (A - B)));
+  for (let i = 1; i <= W; i++) {
+    for (let j = 1; j <= H; j++) {
+      if (i < 10) {
+        arr.push(j + "0" + i);
+      } else {
+        arr.push(`${j}${i}`);
+      }
+    }
+  }
+  console.log(arr[N - 1]);
+}
