f60b6f1a2d34825e718a0bc592568c595dfead2d
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-12 18:19:44 +0900

    level8_9_1011

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
+solution(input);
 
-const T = Number(input.shift());
-for (let k = 0; k < T; k++) {
-  const arr = [];
-  const HWN = input[k].split(" ");
-  const H = Number(HWN[0]); //층
-  const W = Number(HWN[1]); //방
-  const N = Number(HWN[2]); //손님
-
-  for (let i = 1; i <= W; i++) {
-    for (let j = 1; j <= H; j++) {
-      if (i < 10) {
-        arr.push(j + "0" + i);
+function solution(A) {
+  for (let i = 1; i <= +A[0]; i++) {
+    A[i] = A[i].split(" ").map((item) => +item);
+    let n = 0;
+    let distance = A[i][1] - A[i][0];
+    let close = Math.floor(Math.sqrt(distance));
+    let step = 2 * close - 1;
+    let remain = distance - close * close;
+    if (remain !== 0) {
+      if (remain <= close) {
+        step += 1;
       } else {
-        arr.push(`${j}${i}`);
+        step += 2;
       }
     }
+    console.log(step);
   }
-  console.log(arr[N - 1]);
 }
