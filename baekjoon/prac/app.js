8dadd8051cb25aae2238011e428f6031c93bd743
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-13 12:15:42 +0900

    level9_3_11653

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -7,33 +7,31 @@ let input = fs
   .split("\n")
   .map((val) => +val);
 
-const primeNumArr = [];
-let primeNumSum = 0;
+let number = Number(input);
+let originNumber = number; // 원래수를 알기 위해 복사해둠
+let primenumber = 2;
+let smallFactorization = [];
 
-function primeNumber(n) {
-  if (n < 2) {
-    return;
-  }
-
-  for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      return;
+while (true) {
+  if (originNumber === 1) {
+    // 원래 수가 1이면 반복문 탈출
+    break;
+  } else {
+    if (number !== 1) {
+      // number가 1이 아닐때까지
+      if (number % primenumber === 0) {
+        // 소인수분해 되었을 때
+        smallFactorization.push(primenumber);
+        number = number / primenumber;
+      } else {
+        primenumber++;
+      }
+    } else {
+      // 나누어서 number가 1이 되면 반복문을 탈출한다.
+      break;
     }
   }
-  primeNumArr.push(n);
-  primeNumSum += n;
 }
-
-const begin = parseInt(input.shift());
-const end = parseInt(input.shift());
-
-for (let i = begin; i <= end; i++) {
-  primeNumber(i);
-}
-
-if (!primeNumArr.length) {
-  console.log(-1);
-} else {
-  console.log(primeNumSum);
-  console.log(Math.min.apply(null, primeNumArr));
+if (smallFactorization.length !== 0) {
+  console.log(smallFactorization.join("\n"));
 }
