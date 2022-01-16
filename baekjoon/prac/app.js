66def48356f4296c70025288d2d5a3e9de1b218f
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-16 13:06:04 +0900

    level12_5_1427

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -1,3 +1,10 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("")
+  .sort((a, b) => b - a);
+
+console.log(input.join(""));
