ec6a0013fb302d635098635659aafc69571c7a91
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-16 11:13:00 +0900

    level12_1_2750

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -1,60 +1,12 @@
-const { count } = require("console");
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let NM = input.shift().split(" ");
-let N = +NM.shift();
-let M = +NM.shift();
+let N = +input.shift();
+let arr = [];
 
-let white = [
-  "WBWBWBWB",
-  "BWBWBWBW",
-  "WBWBWBWB",
-  "BWBWBWBW",
-  "WBWBWBWB",
-  "BWBWBWBW",
-  "WBWBWBWB",
-  "BWBWBWBW",
-];
-let black = [
-  "BWBWBWBW",
-  "WBWBWBWB",
-  "BWBWBWBW",
-  "WBWBWBWB",
-  "BWBWBWBW",
-  "WBWBWBWB",
-  "BWBWBWBW",
-  "WBWBWBWB",
-];
-
-let board = [];
 for (let i = 0; i < N; i++) {
-  board[i] = input.shift().split("");
+  arr.push(+input[i]);
 }
-
-let answer = 90;
-
-for (let i = 0; i <= N - 8; i++) {
-  for (let j = 0; j <= M - 8; j++) {
-    check(j, i);
-  }
-}
-
-function check(x, y) {
-  let checkWhite = 0;
-  let checkBlack = 0;
-
-  for (let i = y; i < y + 8; i++) {
-    for (let j = x; j < x + 8; j++) {
-      if (board[i][j] !== white[i - y][j - x]) checkWhite++;
-      if (board[i][j] !== black[i - y][j - x]) checkBlack++;
-    }
-  }
-
-  let min = checkBlack < checkWhite ? checkBlack : checkWhite;
-
-  if (min < answer) answer = min;
-}
-
-console.log(answer);
+arr.sort((a, b) => a - b);
+console.log(arr.join("\n"));
