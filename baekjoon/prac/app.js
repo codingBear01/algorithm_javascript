c1cdd04fa1f19dfa17cf42d16c0822944d3255d0
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-17 08:59:58 +0900

    dtd

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("")
-  .sort((a, b) => b - a);
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-console.log(input.join(""));
+const N = +input.shift();
+
+for (let i = 0; i < N; i++) {
+  const [x, y] = input[i].split(" ").map(Number);
+  console.log(x, y);
+}
+// sdafsdafsdaf;
