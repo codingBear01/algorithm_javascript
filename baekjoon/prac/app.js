aa1ee898c0dac04a64eb1236f0154fc47cb4f0c9
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-18 08:42:47 +0900

    level12_7_11651

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -7,8 +7,8 @@ const xy = input.map((v) => v.split(" "));
 
 let answer = "";
 xy.sort((a, b) => {
-  if (a[0] === b[0]) return a[1] - b[1];
-  else return a[0] - b[0];
+  if (a[1] === b[1]) return a[0] - b[0];
+  else return a[1] - b[1];
 }).forEach((arr) => {
   answer += `${arr[0]} ${arr[1]}\n`;
 });
