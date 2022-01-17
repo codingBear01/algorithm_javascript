e8a6031a014222d4d0a07e1ca19b92494f57fd4f
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-18 08:40:38 +0900

    level12_6_11650

--- baekjoon/level12_정렬/level12_6_11650.js
+++ baekjoon/level12_정렬/level12_6_11650.js
@@ -20,3 +20,20 @@
 // 2 2
 // 3 3
 // 3 4
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = +input.shift();
+const xy = input.map((v) => v.split(" "));
+
+let answer = "";
+xy.sort((a, b) => {
+  if (a[0] === b[0]) return a[1] - b[1];
+  else return a[0] - b[0];
+}).forEach((arr) => {
+  answer += `${arr[0]} ${arr[1]}\n`;
+});
+
+console.log(answer);
--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -3,9 +3,14 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const N = +input.shift();
+const xy = input.map((v) => v.split(" "));
 
-for (let i = 0; i < N; i++) {
-  const [x, y] = input[i].split(" ").map(Number);
-  console.log(x, y);
-}
-// sdafsdafsdaf;
+let answer = "";
+xy.sort((a, b) => {
+  if (a[0] === b[0]) return a[1] - b[1];
+  else return a[0] - b[0];
+}).forEach((arr) => {
+  answer += `${arr[0]} ${arr[1]}\n`;
+});
+
+console.log(answer);
