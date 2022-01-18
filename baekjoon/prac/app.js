ac4f57e978c2cc1877e6f9a8ef24285a928a2954
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-18 09:14:25 +0900

    level12_9_10814

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -1,12 +1,15 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
-input.shift();
+const N = +input.shift();
+const xy = input.map((v) => v.split(" "));
 
-let arr = new Set(input);
-arr = [...arr];
+let answer = "";
+xy.sort((a, b) => {
+  if (a[0] === b[0]) return a[1] - b[1];
+  else return a[0] - b[0];
+}).forEach((arr) => {
+  answer += `${arr[0]} ${arr[1]}\n`;
+});
 
-Array.from(new Set(arr))
-  .sort((a, b) => (a > b ? 1 : a < b ? -1 : 0))
-  .sort((a, b) => a.length - b.length)
-  .forEach((i) => console.log(i));
+console.log(answer);
