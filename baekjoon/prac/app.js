2a9f4572feac5d943dca2b6e1961204246d8dd15
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-19 16:16:27 +0900

    level12_10_18870

--- baekjoon/prac/app.js
+++ baekjoon/prac/app.js
@@ -1,10 +1,21 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("")
-  .sort((a, b) => b - a);
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-console.log(input.join(""));
+let N = Number(input.shift());
+let arr = input[0].split(" ").map((x) => +x);
+
+let set = new Set(arr);
+let uniq = [...set].sort((a, b) => a - b);
+
+let dic = {};
+uniq.forEach((e, index) => {
+  dic[e] = index;
+});
+
+let answer = "";
+for (let i = 0; i < arr.length; i++) {
+  answer += dic[arr[i]] + " ";
+}
+
+console.log(answer);
