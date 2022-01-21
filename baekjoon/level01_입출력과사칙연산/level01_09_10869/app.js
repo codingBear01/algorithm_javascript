350ce9199c3bedae65818f0ac71587aa385d6430
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-21 22:24:12 +0900

    level01_08~09

---
+++ baekjoon/level01_입출력과사칙연산/level01_09_10869/app.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map((val) => +val);
+
+const A = input[0];
+const B = input[1];
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(Math.floor(A / B));
+console.log(A % B);
