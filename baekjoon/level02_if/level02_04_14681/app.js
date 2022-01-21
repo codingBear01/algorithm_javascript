aedcffa24c3f9b64aa2646343c231cda1fe35873
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-21 23:12:14 +0900

    level02_04_14681

--- baekjoon/level02_if/level02_04_14681/app.js
+++ baekjoon/level02_if/level02_04_14681/app.js
@@ -1,14 +1,55 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim();
-input = +input;
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-function solution(num) {
-  if ((num % 4 === 0 && num % 100 !== 0) || num % 400 === 0) {
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(parseInt(line));
+}).on("close", function () {
+  const numA = input[0];
+  const numB = input[1];
+
+  function solution(x, y) {
+    if (x > 0 && y > 0) {
+      console.log(1);
+    } else if (x < 0 && y > 0) {
+      console.log(2);
+    } else if (x < 0 && y < 0) {
+      console.log(3);
+    } else if (x > 0 && y < 0) {
+      console.log(4);
+    }
+  }
+
+  solution(numA, numB);
+  process.exit();
+});
+
+/*const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((val) => +val);
+
+const numA = input[0];
+const numB = input[1];
+
+function solution(x, y) {
+  if (x > 0 && y > 0) {
     console.log(1);
-  } else {
-    console.log(0);
+  } else if (x < 0 && y > 0) {
+    console.log(2);
+  } else if (x < 0 && y < 0) {
+    console.log(3);
+  } else if (x > 0 && y < 0) {
+    console.log(4);
   }
 }
 
-solution(input);
+solution(numA, numB);*/
