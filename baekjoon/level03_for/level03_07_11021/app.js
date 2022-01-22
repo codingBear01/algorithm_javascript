1d050b2341f9de8ca5a7c5149bf4085bf90ed686
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-22 09:05:01 +0900

    level03_07_11021

---
+++ baekjoon/level03_for/level03_07_11021/app.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = +input[0];
+
+solution(input);
+
+function solution(nums) {
+  for (let i = 1; i <= N; i++) {
+    const numArr = nums[i].split(" ").map((val) => +val);
+    let A = numArr[0];
+    let B = numArr[1];
+
+    console.log(`Case #${i}: ${A + B}`);
+  }
+}
