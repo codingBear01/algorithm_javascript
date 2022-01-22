3414b64de0b04db84abcb4bae03f7b84847c6206
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-22 09:49:26 +0900

    update_level3_10_2439

--- baekjoon/level03_for/level3_10_2439/app.js
+++ baekjoon/level03_for/level3_10_2439/app.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { listenerCount } = require("process");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
@@ -18,3 +19,19 @@ function solution(N) {
     space = "";
   }
 }
+
+// 라매개발자 ver.
+// function solution(N) {
+//   for (let line = 1; line <= N; line++) {
+//     const starCnt = line;
+//     const spaceCnt = N - line;
+//     let printString = "";
+//     for (let i = 0; i < spaceCnt; i++) {
+//       printString += " ";
+//     }
+//     for (let i = 0; i < starCnt; i++) {
+//       printString += "*";
+//     }
+//     console.log(printString);
+//   }
+// }
