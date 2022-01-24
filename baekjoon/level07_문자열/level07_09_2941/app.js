4f953f665294ba8bbbb358c45dd3b17805441626
codingbearda <95178119+codingbearda@users.noreply.github.com>
2022-01-25 08:58:41 +0900

    level07_09_2941

---
+++ baekjoon/level07_문자열/level07_09_2941/app.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+
+solution(input);
+
+function solution(str) {
+  let ans = 0;
+  for (let i = 0; i < str.length; i++) {
+    let c = str[i];
+    ans++;
+
+    if (i >= str.length - 1) {
+      continue;
+    }
+
+    if (c === "c") {
+      let nextC = str[i + 1];
+      if (nextC === "=" || nextC === "-") {
+        i = i + 1;
+        continue;
+      }
+    }
+
+    if (c === "d") {
+      let nextC = str[i + 1];
+
+      if (nextC === "-") {
+        i = i + 1;
+        continue;
+      }
+
+      if (i < str.length - 2) {
+        let nextNextC = str[i + 2];
+        if (nextC === "z" && nextNextC === "=") {
+          i = i + 2;
+          continue;
+        }
+      }
+    }
+
+    if (c === "l" || c === "n") {
+      let nextC = str[i + 1];
+      if (nextC === "j") {
+        i = i + 1;
+        continue;
+      }
+    }
+
+    if (c === "s" || c === "z") {
+      let nextC = str[i + 1];
+      if (nextC === "=") {
+        i = i + 1;
+        continue;
+      }
+    }
+  }
+  console.log(ans);
+}
