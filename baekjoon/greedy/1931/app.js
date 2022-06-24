bbb647b0b0cbe092b9a028f788bfd6b9ac3ec8f4
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-06-24 10:34:46 +0900

    greedy: 1931

--- baekjoon/greedy/1931/app.js
+++ baekjoon/greedy/1931/app.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const numCnt = +input[0];
-const nums = [];
+const n = +input.shift();
+const list = [];
 
-for (let i = 1; i < input.length; i++) {
-  nums.push(input[i].split(' ').map((val) => +val));
+for (let i = 0; i < input.length; i++) {
+  list.push(input[i].split(' ').map((val) => +val));
 }
 
 function solution(n, list) {
@@ -30,4 +30,4 @@ function solution(n, list) {
   console.log(ans);
 }
 
-solution(numCnt, nums);
+solution(n, list);
