962fb9c01f225e5b75f0cc513229f12f98bcc954
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-06-30 10:01:36 +0900

    level1: 실패율 답안 추가

--- programmers/level_1/실패율/app.js
+++ programmers/level_1/실패율/app.js
@@ -50,28 +50,11 @@ function solution(N, stages) {
     remainedUsers -= challengers;
     answer.push({ stage: i, rate: failureRate });
   }
-  console.log(answer);
+
   answer.sort((a, b) => {
     if (a.rate === b.rate) return a.stage - b.stage;
     else return b.rate - a.rate;
   });
-  console.log(answer);
+
   return answer.map((obj) => obj.stage);
-
-  // let stageRatio = [];
-  // let users = stages.length;
-
-  // for (let i = 1; i <= N; i++) {
-  //   let noClear = stages.filter((user) => i === user).length;
-  //   let ratio = noClear / users;
-  //   users -= noClear;
-  //   stageRatio.push({ stage: i, ratio: ratio });
-  // }
-
-  // stageRatio.sort((a, b) => {
-  //   if (a.ratio === b.ratio) return a.stage - b.stage;
-  //   else return b.ratio - a.ratio;
-  // });
-
-  // return stageRatio.map((obj) => obj.stage);
 }
