b47a94d640514d73827f30885b0406944c61c656
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-06-30 23:52:23 +0900

    Update app.js

--- programmers/level_1/신고_결과_받기/app.js
+++ programmers/level_1/신고_결과_받기/app.js
@@ -12,37 +12,35 @@ const k = 2;
 
 console.log(solution(idList, report, k));
 
-// function solution(id_list, report, k) {
-//   const reportLogs = [...new Set(report)];
-//   const userReportedInfo = {};
-//   const answer = new Array(id_list.length).fill(0);
-
-//   for (const i of id_list) {
-//     if (!userReportedInfo[i]) {
-//       userReportedInfo[i] = [];
-//     }
-//   }
-
-//   reportLogs.forEach((el) => {
-//     const [user, reportedUser] = el.split(' ');
-
-//     if (!userReportedInfo[reportedUser].includes(user)) {
-//       userReportedInfo[reportedUser].push(user);
-//     }
-//   });
-
-//   for (const key in userReportedInfo) {
-//     if (userReportedInfo[key].length >= k) {
-//       userReportedInfo[key].forEach((user) => {
-//         answer[id_list.indexOf(user)]++;
-//       });
-//     }
-//   }
-
-//   return answer;
-// }
-
 function solution(id_list, report, k) {
+  // const reportLogs = [...new Set(report)];
+  // const userReportedInfo = {};
+  // const answer = new Array(id_list.length).fill(0);
+
+  // for (const i of id_list) {
+  //   if (!userReportedInfo[i]) {
+  //     userReportedInfo[i] = [];
+  //   }
+  // }
+
+  // reportLogs.forEach((el) => {
+  //   const [user, reportedUser] = el.split(' ');
+
+  //   if (!userReportedInfo[reportedUser].includes(user)) {
+  //     userReportedInfo[reportedUser].push(user);
+  //   }
+  // });
+
+  // for (const key in userReportedInfo) {
+  //   if (userReportedInfo[key].length >= k) {
+  //     userReportedInfo[key].forEach((user) => {
+  //       answer[id_list.indexOf(user)]++;
+  //     });
+  //   }
+  // }
+
+  // return answer;
+
   const userList = id_list.reduce((result, currentId) => {
     result[currentId] = [0, []];
     return result;
@@ -56,11 +54,9 @@ function solution(id_list, report, k) {
   }
 
   const bannedUsers = id_list.filter((id) => userList[id][0] >= k);
-  const emailCnts = id_list.map((id) => {
-    return userList[id][1].filter((id) => {
-      return bannedUsers.includes(id);
-    }).length;
-  });
+  const emailCnts = id_list.map(
+    (id) => userList[id][1].filter((id) => bannedUsers.includes(id)).length
+  );
 
   return emailCnts;
 }
