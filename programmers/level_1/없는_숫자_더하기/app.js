36a10c19ac9aca8c2a906d0382f72447a717436e
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-01 19:38:52 +0900

    level1: 없는 숫자 더하기

---
+++ programmers/level_1/없는_숫자_더하기/app.js
@@ -0,0 +1,13 @@
+const input = [1, 2, 3, 4, 6, 7, 8, 0];
+// const input = [5,8,4,0,6,7,9]
+
+console.log(solution(input));
+
+function solution(numbers) {
+  let answer = 0;
+
+  for (let i = 0; i <= 9; i++) {
+    if (!numbers.includes(i)) answer += i;
+  }
+  return answer;
+}
