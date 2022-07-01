73ab15abe1453c5a1cd3a2501389a4d78ff29f4b
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-01 17:56:24 +0900

    update 오픈채팅방

--- programmers/level_2/오픈채팅방/app.js
+++ programmers/level_2/오픈채팅방/app.js
@@ -10,22 +10,22 @@ console.log(solution(input));
 
 function solution(record) {
   /* Map Ver. */
-  // let answer = [];
-  // const map = new Map();
-  // for (const info of record) {
-  //   const [action, id, nickname] = info.split(' ');
-  //   if (action === 'Leave') {
-  //     answer.push([id, '님이 나갔습니다.']);
-  //     continue;
-  //   }
-  //   if (action === 'Enter') {
-  //     answer.push([id, '님이 들어왔습니다.']);
-  //   }
-  //   map.set(id, nickname);
-  // }
-  // return answer.map((el) => map.get(el[0]) + el[1]);
+  const answer = [];
+  const map = new Map();
+  for (const info of record) {
+    const [action, id, nickname] = info.split(' ');
+    if (action === 'Leave') {
+      answer.push([id, '님이 나갔습니다.']);
+      continue;
+    }
+    if (action === 'Enter') {
+      answer.push([id, '님이 들어왔습니다.']);
+    }
+    map.set(id, nickname);
+  }
+  return answer.map((el) => map.get(el[0]) + el[1]);
   /* Obj Ver. */
-  // let answer = [];
+  // const answer = [];
   // const obj = {};
   // for (const info of record) {
   //   const [action, id, nickname] = info.split(' ');
@@ -39,19 +39,33 @@ function solution(record) {
   //   obj[id] = nickname;
   // }
   // return answer.map((el) => obj[el[0]] + el[1]);
-
   /* Simple Ver.*/
-  const users = {};
-  const info = [];
-  const actObj = {
-    Enter: '님이 들어왔습니다.',
-    Leave: '님이 나갔습니다.',
-  };
-
-  for (const rec of record) {
-    const [act, id, nick] = rec.split(' ');
-    if (nick) users[id] = nick;
-    if (act !== 'Change') info.push([act, id]);
-  }
-  return info.map(([act, id]) => `${users[id]}${actObj[act]}`);
+  // const users = {};
+  // const info = [];
+  // const actObj = {
+  //   Enter: '님이 들어왔습니다.',
+  //   Leave: '님이 나갔습니다.',
+  // };
+  // for (const rec of record) {
+  //   const [act, id, nick] = rec.split(' ');
+  //   if (nick) users[id] = nick;
+  //   if (act !== 'Change') info.push([act, id]);
+  // }
+  // return info.map(([act, id]) => `${users[id]}${actObj[act]}`);
+  /* filter method Ver. */
+  // const userName = {};
+  // const message = {
+  //   Leave: '나갔습니다.',
+  //   Enter: '들어왔습니다.',
+  // };
+  // return record
+  //   .map((v) => v.split(' '))
+  //   .filter((v) => {
+  //     const [state, userId, name] = v;
+  //     if (name) {
+  //       userName[userId] = name;
+  //     }
+  //     return state !== 'Change';
+  //   })
+  //   .map((v) => `${userName[v[1]]}님이 ${message[v[0]]}`);
 }
