92e8bc6dfe35d2b11067ad3510bf6926324533f3
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-03 12:05:02 +0900

    스파로스 아카데미

--- programmers/level_1/소수_만들기/app.js
+++ programmers/level_1/소수_만들기/app.js
@@ -4,40 +4,61 @@ const input = [1, 2, 3, 4];
 console.log(solution(input));
 
 function solution(nums) {
-  const result = getCombinations(nums, 3);
-  const combinations = [...result];
-  const answer = [];
+  /* Recursion Ver.*/
+  //   const result = getCombinations(nums, 3);
+  //   const combinations = [...result];
+  //   const answer = [];
+  //   for (let i = 0; i < combinations.length; i++) {
+  //     let sum = 0;
+  //     for (let j = 0; j < combinations[i].length; j++) {
+  //       sum += combinations[i][j];
+  //     }
+  //     let remain = 0;
+  //     for (let j = 1; j <= sum; j++) {
+  //       if (sum % j === 0) {
+  //         remain++;
+  //       }
+  //     }
+  //     if (remain === 2) answer.push(sum);
+  //   }
+  //   return answer.length;
+  // }
+  // function getCombinations(arr, selectNumber) {
+  //   const results = [];
+  //   if (selectNumber === 1) return arr.map((value) => [value]);
+  //   // 1개씩 택할 때, 바로 모든 배열의 원소 return
+  //   arr.forEach((fixed, index, origin) => {
+  //     const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
+  //     const combinations = getCombinations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
+  //     const attached = combinations.map((combination) => [fixed, ...combination]);
+  //     //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+  //     results.push(...attached); // 배열 spread syntax 로 모두다 push
+  //   });
+  //   return results; // 결과 담긴 results return
 
-  for (let i = 0; i < combinations.length; i++) {
-    let sum = 0;
+  /* For Loop Ver. */
+  let len = nums.length,
+    answer = 0;
 
-    for (let j = 0; j < combinations[i].length; j++) {
-      sum += combinations[i][j];
-    }
-
-    let remain = 0;
-    for (let j = 1; j <= sum; j++) {
-      if (sum % j === 0) {
-        remain++;
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      for (let k = j + 1; k < len; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          answer++;
+        }
       }
     }
-    if (remain === 2) answer.push(sum);
   }
-  return answer.length;
+
+  return answer;
 }
 
-function getCombinations(arr, selectNumber) {
-  const results = [];
-  if (selectNumber === 1) return arr.map((value) => [value]);
-  // 1개씩 택할 때, 바로 모든 배열의 원소 return
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
 
-  arr.forEach((fixed, index, origin) => {
-    const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
-    const combinations = getCombinations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
-    const attached = combinations.map((combination) => [fixed, ...combination]);
-    //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
-    results.push(...attached); // 배열 spread syntax 로 모두다 push
-  });
-
-  return results; // 결과 담긴 results return
+  return true;
 }
--- programmers/코딩테스트/2022_스파로스_아카데미/app.js
+++ programmers/코딩테스트/2022_스파로스_아카데미/app.js
@@ -3,8 +3,34 @@ const input = [1, 1, 3, 3, 2, 2, 4, 5, 1, 1, 1, 3, 3, 3]; // 6
 // const input = [2, 2, 1, 1, 2, 2, 1, 1, 2, 2, 2, 1, 2]; // 5
 // const input = [2]; // 1
 
+// [1, 1, 3, 3, 2, 2, 4, 5, 1, 1, 1, 3, 3, 3] -> [2, 2, 2, 1, 1, 3, 3] -> [3, 2, 2] -> [1, 2] -> [1, 1] -> [2] -> [1] 줄이기 작업 6회 수행
+
 console.log(solution(input));
 
 function solution(nums) {
-  return nums;
+  let answer = 0;
+
+  function getNumArr(nums) {
+    let matchedCnts = 0;
+    let checkingNum = 0;
+    const arr = [];
+    answer++;
+
+    for (let i = 0; i <= nums.length; i++) {
+      if (checkingNum !== nums[i]) {
+        if (matchedCnts > 0) {
+          arr.push(matchedCnts);
+        }
+        checkingNum = nums[i];
+        matchedCnts = 1;
+      } else {
+        matchedCnts++;
+      }
+    }
+    // arr의 값이 최초로 1이 되는 순간 반환
+    if (arr.length === 1 && arr[0] === 1) return answer;
+    else return getNumArr(arr);
+  }
+
+  return getNumArr(nums);
 }
