6dd6c2f34c5291efe1ccfc7789697f2dfc9edf37
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-06 10:01:45 +0900

    level 1: 폰켓몬

--- programmers/level_1/체육복/app.js
+++ programmers/level_1/체육복/app.js
@@ -26,44 +26,62 @@ function solution(n, lost, reserve) {
   // }
   // return answer
   /* Filter Ver. */
-  // let realLost = lost
-  //   .filter((el) => !reserve.includes(el))
+  // 여별 체육복을 가진 학생과 체육복을 도난 당한 학생 모두에 속하는 사람을 제외
+  // const realLost = lost
+  //   .filter((l) => !reserve.includes(l))
   //   .sort((a, b) => a - b);
   // let realReserve = reserve
-  //   .filter((el) => !lost.includes(el))
+  //   .filter((r) => !lost.includes(r))
   //   .sort((a, b) => a - b);
+  // // 체육 수업을 들을 수 있는 학생 수 = 전체 학생 수 - 체육복 없는 학생 중 체육복을 빌리지 못하는 학생 수
   // return (
   //   n -
-  //   realLost.filter((lostEl) => {
-  //     let abs = realReserve.find(
-  //       (reserveEl) => Math.abs(lostEl - reserveEl) === 1
-  //     );
-  //     if (!abs) return true;
-  //     realReserve = realReserve.filter((reserveEl) => reserveEl !== abs);
+  //   realLost.filter((l) => {
+  //     // 여별 체육복을 가진 학생 중 도난 당한 학생과 체격 차이가 1인 학생
+  //     const lendStudent = realReserve.find((r) => {
+  //       return Math.abs(l - r) === 1;
+  //     });
+  //     // 빌려주는 학생과 빌리는 학생의 체격 차이가 1이 아닌 경우 그대로 realLost에 남김
+  //     if (!lendStudent) return true;
+  //     // 여벌 체육복은 한 학생에게만 빌려줄 수 있으므로 이미 체육복을 빌려준 학생은 다음 계산에서는 제외
+  //     realReserve = realReserve.filter((r) => r !== lendStudent);
   //   }).length
   // );
-  /* Plus & Minus Ver. */
-  //   // 모든 student가 체육복을 1개씩 갖고 있는 것으로 가정한 배열.
-  //   var students = new Array(n).fill(1);
-  //   // 체육복을 잃어버린 student의 체육복 개수를 -1 감소
-  // lost.forEach(l => students[l - 1]--);
-  //   // 여분 체육복이 있는 student의 체육복 개수를 +1 증가
-  // reserve.forEach(r => students[r - 1]++);
-  //   /*
-  // 여분 체육복이 있는 student(체육복이 2개 이상인 student)가 체육복이 없는 앞, 뒤 student에게 체육복을 준다.
-  //   이 때 앞, 뒤 student 둘 다 체육복이 없는 경우 앞의 student에게 체육복을 준다.
-  // (배열을 앞->뒤 순서로 순회하며 체육복을 나눠주기 때문에, 앞의 student에게 우선적으로 체육복을 나눠주어야
-  // 가장 많은 student에게 체육복을 나눠줄 수 있음.) => 선택의 순간마다 최적의 알고리즘 적용 (Greedy)
-  // */
-  // students.forEach((_, idx, arr) => {
-  //     if (arr[idx] > 1 && arr[idx - 1] === 0) {
-  //         arr[idx]--;
-  //         arr[idx - 1]++;
-  //     } else if (arr[idx] > 1 && arr[idx + 1] === 0) {
-  //         arr[idx]--;
-  //         arr[idx + 1]++;
-  //     }
-  // })
-  // console.log(students)
-  // return students.filter(v => v > 0).length;
+  /* New Array Ver.*/
+  // // 모든 학생이 체육복을 1개씩 갖고 있다고 가정
+  // let students = new Array(n).fill(1);
+  // // 체육복을 잃어버린 학생의 체육복 개수를 --
+  // lost.forEach((l) => students[l - 1]--);
+  // // 체육복 여벌이 있는 학생의 체육복 개수 ++
+  // reserve.forEach((r) => students[r - 1]++);
+  // // students 배열을 순회하면서 여분 체육복이 있는 학생이 체육복이 없는 앞, 뒤의 학생에게 자신의 체육복을 나눠준다. 이때 앞 -> 뒤로 순회하기 때문에 앞에 있는 학생부터 나눠준다.
+  // students.forEach((_, i, arr) => {
+  //   if (arr[i] > 1 && arr[i - 1] === 0) {
+  //     arr[i]--;
+  //     arr[i - 1]++;
+  //   } else if (arr[i] > 1 && arr[i + 1] === 0) {
+  //     arr[i]--;
+  //     arr[i + 1]++;
+  //   }
+  // });
+  // return students.filter((v) => v > 0).length;
+  let answer = n - lost.length;
+  let realLost = lost.filter((l) => !reserve.includes(l)).sort((a, b) => a - b);
+  let realReserve = reserve
+    .filter((r) => !lost.includes(r))
+    .sort((a, b) => a - b);
+  // 체육 수업 들을 수 있는 학생 수 = 전체 학생 수 - 체육복 갖고 있지 않은 학생 수
+  answer += lost.length - realLost.length;
+  // 여벌의 체육복을 가진 학생 중 체육복을 도난 당한 학생이 있다면 realReserve 배열에서 제외하고 answer ++
+  realLost.forEach((l) => {
+    if (realReserve.length === 0) return;
+    if (realReserve.includes(l - 1)) {
+      realReserve = realReserve.filter((r) => r !== l - 1);
+      answer++;
+    } else if (realReserve.includes(l + 1)) {
+      realReserve = realReserve.filter((r) => r !== l + 1);
+      answer++;
+    }
+  });
+  // return answer;
 }
---
+++ programmers/level_1/폰켓몬/app.js
@@ -0,0 +1,51 @@
+// const nums = [3, 1, 2, 3];
+// const nums = [3,3,3,2,2,4]
+const nums = [3, 3, 3, 2, 2, 2];
+
+console.log(solution(nums));
+
+function solution(nums) {
+  /* Solution 1. - My Solution */
+  let answer = [];
+  const n = nums.length / 2;
+
+  for (let i = 0; i < nums.length; i++) {
+    while (answer.length < n) {
+      if (!answer.includes(nums[i])) {
+        answer.push(nums[i]);
+      } else if (answer.includes(nums[i])) {
+        i++;
+      }
+    }
+  }
+
+  return answer.filter((ans) => ans !== undefined).length;
+
+  /* Solution 2. */
+  // const answer = [];
+  // const n = nums.length / 2;
+
+  // for (const num of nums) {
+  //   if (answer.length < n) {
+  //     if (!answer.includes(num)) answer.push(num);
+  //   }
+  // }
+
+  // return answer.length;
+
+  /* Solution 3. - using map() */
+  // const answer = [];
+  // const n = nums.length / 2;
+
+  // nums.map((num) =>
+  //   answer.length < n && !answer.includes(num) ? answer.push(num) : num
+  // );
+
+  // return answer.length;
+
+  /* Solution 4. - using Set */
+  // const answer = [...new Set(nums)],
+  //   n = nums.length / 2;
+
+  // return answer.length > n ? n : answer.length;
+}
