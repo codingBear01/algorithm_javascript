c06763ecd9c8db2c8ee2f90ba77dc0d158c7e748
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-06 10:27:31 +0900

    update 폰켓몬

--- programmers/level_1/폰켓몬/app.js
+++ programmers/level_1/폰켓몬/app.js
@@ -6,20 +6,20 @@ console.log(solution(nums));
 
 function solution(nums) {
   /* Solution 1. - My Solution */
-  let answer = [];
-  const n = nums.length / 2;
+  // let answer = [];
+  // const n = nums.length / 2;
 
-  for (let i = 0; i < nums.length; i++) {
-    while (answer.length < n) {
-      if (!answer.includes(nums[i])) {
-        answer.push(nums[i]);
-      } else if (answer.includes(nums[i])) {
-        i++;
-      }
-    }
-  }
+  // for (let i = 0; i < nums.length; i++) {
+  //   while (answer.length < n) {
+  //     if (!answer.includes(nums[i])) {
+  //       answer.push(nums[i]);
+  //     } else if (answer.includes(nums[i])) {
+  //       i++;
+  //     }
+  //   }
+  // }
 
-  return answer.filter((ans) => ans !== undefined).length;
+  // return answer.filter((ans) => ans !== undefined).length;
 
   /* Solution 2. */
   // const answer = [];
@@ -44,8 +44,30 @@ function solution(nums) {
   // return answer.length;
 
   /* Solution 4. - using Set */
-  // const answer = [...new Set(nums)],
+  // const answer = new Set(nums),
   //   n = nums.length / 2;
 
-  // return answer.length > n ? n : answer.length;
+  // return answer.size > n ? n : answer.size;
+
+  /* Solution 5. - indexOf() */
+  // const n = nums.length / 2;
+  // const phonekemons = nums.filter((v, i, t) => t.indexOf(v) === i);
+
+  // return phonekemons.length < n ? phonekemons.length : n;
+
+  /* Solution 6. - Object */
+  const phonekemons = {};
+  const n = nums.length / 2;
+
+  for (const num of nums) {
+    if (!phonekemons[num]) {
+      phonekemons[num] = 1;
+    } else {
+      phonekemons[num]++;
+    }
+  }
+
+  return Object.keys(phonekemons).length > n
+    ? n
+    : Object.keys(phonekemons).length;
 }
