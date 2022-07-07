40679d23c3b636d03001b0d4668ee29205730ebb
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-07 13:16:57 +0900

    level 1: 두 개 뽑아서 더하기

---
+++ programmers/level_1/두_개_뽑아서_더하기/app.js
@@ -0,0 +1,31 @@
+const numbers = [2, 1, 3, 4, 1];
+// const numbers = [5,0,2,7]
+
+console.log(solution(numbers));
+
+function solution(numbers) {
+  /* My Solution */
+  const answer = [];
+  numbers.sort((a, b) => a - b);
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      const sum = numbers[i] + numbers[j];
+      if (!answer.includes(sum)) answer.push(sum);
+    }
+  }
+  return answer.sort((a, b) => a - b);
+
+  /* Set Ver. */
+  // let answer = [];
+  // numbers.sort((a, b) => a - b);
+
+  // for (let i = 0; i < numbers.length; i++) {
+  //   for (let j = i + 1; j < numbers.length; j++) {
+  //     const sum = numbers[i] + numbers[j];
+  //     answer.push(sum);
+  //   }
+  // }
+
+  // return (answer = [...new Set(answer)].sort((a, b) => a - b));
+}
