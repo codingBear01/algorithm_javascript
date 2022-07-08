21f64b0d26c40cf35e69773a7d44d651b51f5b84
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-09 07:55:08 +0900

    level 1: 나머지가 1이 되는 수 찾기

---
+++ programmers/level_1/나머지가_1이_되는_수_찾기/app.js
@@ -0,0 +1,11 @@
+const n = 10;
+// const n = 12
+
+console.log(solution(n));
+
+function solution(n) {
+  /* My Solutions - Loop Ver. */
+  //   for (let i = 1; i <= n; i++) if (n % i === 1) return i;
+  // let answer = 1;
+  // while (answer++) if (n % answer === 1) return answer;
+}
