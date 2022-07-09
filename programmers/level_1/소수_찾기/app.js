c1860f9c5f775ef3a72bbab310d72ad93fe19309
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-10 08:05:22 +0900

    update 소수 찾기

--- programmers/level_1/소수_찾기/app.js
+++ programmers/level_1/소수_찾기/app.js
@@ -1,20 +1,34 @@
-const n = 10
+const n = 10;
 // const n = 5
 
-console.log(solution(n))
+console.log(solution(n));
 
 function solution(n) {
-  if (n === 2) return 1
-  let answer = 1;
-  for (let i = 3; i <= n; i++) {
-      const isPrime = primeChecker(i)
-      if (isPrime) answer++
+  // if (n === 2) return 1
+  // let answer = 1;
+  // for (let i = 3; i <= n; i++) {
+  //     const isPrime = primeChecker(i)
+  //     if (isPrime) answer++
+  // }
+  // return answer;
+
+  /* Eratosthenes' Sieve*/
+  let answer = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+  // Array()에서 괄호 안의 숫자는 인덱스이므로 주어진 수까지 채우려면 n + 1 해줘야 함.
+  // 0과 1은 소수가 아니므로 false 처리
+
+  for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+    if (answer[i]) for (let j = i * i; j <= n; j += i) answer[j] = false;
   }
-  return answer;
+
+  return answer.filter((val) => val === true).length;
 }
 
 function primeChecker(i) {
-  if (i % 2 === 0) return false
-  for (let j = 3; j <= Math.floor(Math.sqrt(i)); j++) if (i % j === 0) return false
-  return true
+  if (i % 2 === 0) return false;
+  for (let j = 3; j <= Math.floor(Math.sqrt(i)); j++)
+    if (i % j === 0) return false;
+  return true;
 }
