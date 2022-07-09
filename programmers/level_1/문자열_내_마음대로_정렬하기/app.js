53c23d09f0396a1213d506b0a6bc167b6fb54709
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-09 19:22:31 +0900

    update 문자열 내 마음대로 정렬하기

--- programmers/level_1/문자열_내_마음대로_정렬하기/app.js
+++ programmers/level_1/문자열_내_마음대로_정렬하기/app.js
@@ -6,23 +6,21 @@ const n = 1;
 console.log(solution(strings, n));
 
 function solution(strings, n) {
-  function solution(strings, n) {
-    /* sort() Ver. */
-    strings.sort((a, b) => {
-      if (a[n] > b[n]) return 1;
-      if (a[n] < b[n]) return -1;
-      if (a[n] === b[n])
-        if (a > b) return 1;
-        else return -1;
-      return 0;
+  /* sort() Ver. */
+  strings.sort((a, b) => {
+    if (a[n] > b[n]) return 1;
+    if (a[n] < b[n]) return -1;
+    if (a[n] === b[n])
+      if (a > b) return 1;
+      else return -1;
+    return 0;
 
-      /* Using Boolean */
-      // if (first === second) return (a > b) - (a < b)
-      // else return (first > second) - (first < second)
-    });
-    return strings;
+    /* Using Boolean */
+    // if (first === second) return (a > b) - (a < b)
+    // else return (first > second) - (first < second)
+  });
+  return strings;
 
-    /* localeCompare() Ver. */
-    // return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]))
-  }
+  /* localeCompare() Ver. */
+  // return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]))
 }
