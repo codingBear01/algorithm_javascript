7041a49cfae6879a6cc0d89450b1a36d89e7ac2b
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-09 13:06:35 +0900

    level 1: 두 정수 사이의 합

---
+++ programmers/level_1/두_정수_사이의_합/app.js
@@ -0,0 +1,27 @@
+const a = 3;
+const b = 5;
+// const a = 3
+// const b = 3
+// const a = 5
+// const b = 3
+
+console.log(solution(a, b));
+
+function solution(a, b) {
+  let answer = 0;
+  let num1;
+  let num2;
+  if (a > b) {
+    num1 = b;
+    num2 = a;
+  } else {
+    num1 = a;
+    num2 = b;
+  }
+  /* for loop Ver. */
+  // for (let i = num1; i <= num2; i++) answer += i
+
+  // return answer;
+  /* Gauss Ver. */
+  return ((num1 + num2) * (num2 - num1 + 1)) / 2;
+}
