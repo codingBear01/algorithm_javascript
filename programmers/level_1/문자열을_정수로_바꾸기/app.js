c754c31817ea452d54e1d9a8416b1be3e62d9b1d
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-10 08:59:17 +0900

    level 1: 문자열을 정수로 바꾸기

--- programmers/level_1/문자열을_정수로_바꾸기/app.js
+++ programmers/level_1/문자열을_정수로_바꾸기/app.js
@@ -5,10 +5,10 @@ console.log(solution(s));
 
 function solution(s) {
   /* My Solution */
-  // if (s[0] === '-') return s.slice(1) * -1
-  // else return +s
+  if (s[0] === '-') return s.slice(1) * -1;
+  else return +s;
 
   /* Simple Ver. */
-  // return +s
-  return s / 1;
+  // return +s;
+  // return s / 1;
 }
