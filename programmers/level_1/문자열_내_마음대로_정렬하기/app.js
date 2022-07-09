b7fc5f93b4113600446816e03e9f08e2c59e721d
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-09 19:00:45 +0900

    level1: 문자열 내 마음대로 정렬하기

---
+++ programmers/level_1/문자열_내_마음대로_정렬하기/app.js
@@ -0,0 +1,28 @@
+const strings = ['sun', 'bed', 'car'];
+const n = 1;
+// const strings = ["abce", "abcd", "cdx"]
+// const n = 2
+
+console.log(solution(strings, n));
+
+function solution(strings, n) {
+  function solution(strings, n) {
+    /* sort() Ver. */
+    strings.sort((a, b) => {
+      if (a[n] > b[n]) return 1;
+      if (a[n] < b[n]) return -1;
+      if (a[n] === b[n])
+        if (a > b) return 1;
+        else return -1;
+      return 0;
+
+      /* Using Boolean */
+      // if (first === second) return (a > b) - (a < b)
+      // else return (first > second) - (first < second)
+    });
+    return strings;
+
+    /* localeCompare() Ver. */
+    // return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]))
+  }
+}
