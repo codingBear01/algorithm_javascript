5552d15fd038a9a6f618fe91fb66db8f9be1b336
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-10 14:18:58 +0900

    level 1: 최대공약수와 최대공배수

---
+++ programmers/level_1/최대공약수와_최대공배수/app.js
@@ -0,0 +1,37 @@
+const n = 3;
+const m = 12;
+// const n = 2
+// const m = 5
+
+console.log(solution(n, m));
+
+function solution(n, m) {
+  /* Loop Ver. */
+  // const gcd = getGcd(n, m);
+  // const lcm = getLcm(n, m);
+  // return [gcd, lcm];
+
+  /* Euclidean Algorithm */
+  const gcd = (n, m) => (n % m === 0 ? m : gcd(m, n % m));
+  const lcm = (n, m) => (n * m) / gcd(n, m);
+  return [gcd(n, m), lcm(n, m)];
+}
+
+function getGcd(n, m) {
+  // 최대공약수
+  let gcd = 1;
+  for (let i = 2; i <= Math.min(n, m); i++) {
+    if (n % i === 0 && m % i === 0) gcd = i;
+  }
+  return gcd;
+}
+
+function getLcm(n, m) {
+  // 최소공배수
+  let lcm = 1;
+  while (true) {
+    if (lcm % n === 0 && lcm % m === 0) break;
+    lcm++;
+  }
+  return lcm;
+}
